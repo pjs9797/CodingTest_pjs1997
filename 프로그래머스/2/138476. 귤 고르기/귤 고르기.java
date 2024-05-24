bbd69e06f4da932862443b219f347583bf33ef58
pjs9797 <99011004+pjs9797@users.noreply.github.com>
2024-05-24 23:30:16 +0900

    [level 2] Title: 귤 고르기, Time: 53.94 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,31 +1,35 @@
-import java.io.*;
 import java.util.*;
+
 class Solution {
-        public int solution(int k, int[] tangerine) {
-            int answer = 0;
-            int z=k;
-            Map<Integer,Integer> map = new HashMap<>();
-            for (int i = 0; i < tangerine.length; i++) {
-                if(map.containsKey(tangerine[i])){
-                    map.put(tangerine[i],map.get(tangerine[i])+1);
-                }
-                else{
-                    map.put(tangerine[i], 1);
-                }
+    public int solution(int k, int[] tangerine) {
+        int max = k;
+        int answer = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < tangerine.length; i++) {
+            if(map.containsKey(tangerine[i])){
+                map.put(tangerine[i],map.get(tangerine[i])+1);
             }
-            List<Map.Entry<Integer,Integer>> entries = new ArrayList<>(map.entrySet());
-            entries.sort((a,b)->{
-                return b.getValue().compareTo(a.getValue());
-            });
-
-            for (Map.Entry<Integer,Integer> entry : entries){
-                z-=entry.getValue();
-                answer += 1;
-                if(z<=0){
-                    break;
-                }
+            else{
+                map.put(tangerine[i],1);
             }
-
-            return answer;
         }
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+
+        Collections.sort(list,(b,a)->{
+            return a.getValue()-b.getValue();
+        });
+        
+        for (Map.Entry<Integer,Integer> entry: list){
+            if(max-entry.getValue()<=0){
+                answer += 1;
+                break;
+            }
+            else {
+                max -= entry.getValue();
+                answer+=1;
+            }
+        }
+
+        return answer;
     }
+}
