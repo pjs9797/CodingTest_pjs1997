3473ba2633674f7f6b75271bd016d81d06ee2f11
pjs9797 <99011004+pjs9797@users.noreply.github.com>
2024-09-16 16:12:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.19 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,51 @@
 import java.util.*;
+
 class Solution {
+    static int [] dx = {1,-1,0,0};
+    static int [] dy = {0,0,-1,1};
+    
     public int solution(int[][] maps) {
         int answer = -1;
-        Queue<Node> queue = new LinkedList<>();
         int n = maps.length;
         int m = maps[0].length;
-        int [] dx = {-1,1,0,0};
-        int [] dy = {0,0,-1,1};
-        boolean [][] visited = new boolean[n][m];
-        visited[0][0] = true;
+        Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(0,0,1));
-        while (!queue.isEmpty()){
+        boolean[][] visited = new boolean[n][m];
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()){
             Node node = queue.poll();
-            if (node.x == n-1 && node.y == m-1){
-                answer = node.cnt;
+            int x = node.x;
+            int y = node.y;
+            int cnt = node.cnt;
+            if (x==n-1 && y == m-1){
+                answer = cnt;
                 break;
             }
-            for (int i = 0; i < 4; i++) {
-                int ax = node.x+dx[i];
-                int ay = node.y+dy[i];
-                if (ax>=0 && ay>=0 && ax < n && ay < m){
-                    if (!visited[ax][ay] && maps[ax][ay] == 1) {
-                        queue.add(new Node(ax, ay, node.cnt + 1));
+            for(int i=0;i<4;i++){
+                int ax = x+dx[i];
+                int ay = y+dy[i];
+                if(ax>=0 && ay>=00 && ax<n && ay<m && maps[ax][ay] == 1){
+                    if(!visited[ax][ay]){
+                        queue.add(new Node(ax,ay,cnt+1));
                         visited[ax][ay] = true;
                     }
                 }
             }
         }
+        
+        
         return answer;
     }
-
+    
     static class Node{
         int x;
         int y;
         int cnt;
-        Node(int x, int y,int cnt){
-            this.x = x;
-            this.y = y;
+        
+        public Node(int x, int y, int cnt){
+            this.x=x;
+            this.y=y;
             this.cnt = cnt;
         }
     }
