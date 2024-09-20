e858250c4eb274a7579f4eca82867d7a79dc2b52
pjs9797 <99011004+pjs9797@users.noreply.github.com>
2024-09-20 22:41:53 +0900

    [level 2] Title: 무인도 여행, Time: 59.69 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,76 +1,77 @@
 import java.util.*;
+
 class Solution {
-    public static int[] solution(String[] maps) {
+    
+    static int [] dx = {-1,1,0,0};
+    static int [] dy = {0,0,1,-1};
+    public int[] solution(String[] maps) {
         int[] answer = {};
-        ArrayList<Integer> list = new ArrayList<>();
-        int [][] map = new int[maps.length][maps[0].length()];
-        int n = map.length;
-        int m = map[0].length;
-        Queue<Node> queue = new LinkedList<>();
-        boolean [][] visited = new boolean[n][m];
-        int [] dx = {-1,1,0,0};
-        int [] dy = {0,0,-1,1};
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length(); j++) {
-                if (maps[i].charAt(j) == 'X'){
-                    map[i][j] = -1;
+        String[][] arr = new String[maps.length][maps[0].length()];
+        boolean [][] visited = new boolean[maps.length][maps[0].length()];
+        
+        for(int i = 0;i<arr.length;i++){
+            String [] A = maps[i].split("");
+            for(int j=0;j<A.length;j++){
+                arr[i][j] = A[j];
+                if(A[j].equals("X")){
                     visited[i][j] = true;
-                }
-                else {
-                    map[i][j] = Integer.parseInt(String.valueOf(maps[i].charAt(j)));
                 }
             }
         }
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                int count = 0;
-                if (map[i][j] != -1 && !visited[i][j]){
-                    queue.add(new Node(i,j,map[i][j]));
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(!arr[i][j].equals("X") && !visited[i][j]){
+                    int cnt = 0;
+                    Queue<Node> queue = new LinkedList<>();
+                    queue.add(new Node(i,j));
+                    cnt += Integer.parseInt(arr[i][j]);
+                    System.out.println(i+" "+j+" "+cnt);
                     visited[i][j] = true;
-                    while (!queue.isEmpty()){
+                    while(!queue.isEmpty()){
                         Node node = queue.poll();
-                        map[node.x][node.y] = -1;
-                        count += node.cnt;
-                        for (int k = 0; k < 4; k++) {
+                        for(int k=0;k<4;k++){
                             int ax = node.x+dx[k];
                             int ay = node.y+dy[k];
-                            if (ax>=0 && ay>=0 && ax<n && ay<m){
-                                if (map[ax][ay] != -1 && !visited[ax][ay]){
-                                    queue.add(new Node(ax,ay,map[ax][ay]));
+                            if(ax>=0 && ay>=0 && ax<arr.length && ay<arr[0].length){
+                                if(!visited[ax][ay]){
+                                    queue.add(new Node(ax,ay));
+                                    cnt += Integer.parseInt(arr[ax][ay]);
+                                    System.out.println(node.x+" "+node.y+" "+cnt);
                                     visited[ax][ay] = true;
                                 }
                             }
                         }
                     }
-                }
-                if (count != 0) {
-                    list.add(count);
+                    list.add(cnt);
                 }
             }
         }
-
-        Collections.sort(list);
-        if (list.isEmpty()){
-            int [] a = {-1};
-            return a;
+        
+        Collections.sort(list,(o1,o2)->{
+            return o1-o2;
+        });
+        if(list.isEmpty()){
+            answer = new int[]{-1};
         }
-        else {
+        else{
             answer = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
+            for(int i=0;i<list.size();i++){
                 answer[i] = list.get(i);
             }
-            return answer;
         }
+        return answer;
     }
-
-    static class Node{
+    
+    public static class Node{
         int x;
         int y;
-        int cnt;
-        Node(int x, int y, int cnt){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
+        
+        public Node(int x,int y){
+            this.x=x;
+            this.y=y;
         }
     }
 }
